34b22b9d036fcc366a975298a19098f944851bd1
m769m <m_769_m@vk.com>
2022-07-28 20:51:45 +0700

    commit

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const width = window.innerWidth
-    if (width <= 768) {
-        new Swiper('.repair__list', {
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            slidesPerView: 'auto'
-        });
-    }
-})
-
-
+new Swiper('.repair__list', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 'auto'
+});
